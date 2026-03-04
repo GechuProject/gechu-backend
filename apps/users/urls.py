020796d6654464f10f79d00978fd2880ff7569bd
ds8000 <ds8600@naver.com>
2026-03-04 16:03:38 +0900

    feat(auth): connect email jwt login endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.users.views.jwt import EmailTokenObtainPairView
 
 urlpatterns = [
-    path("auth/login", TokenObtainPairView.as_view(), name="auth-login"),
+    path("auth/login", EmailTokenObtainPairView.as_view(), name="auth-login"),
     path("auth/refresh", TokenRefreshView.as_view(), name="auth-refresh"),
 ]
