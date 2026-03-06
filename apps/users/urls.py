835752608d16df869eb48457426a14fdc601f7fe
ds8000 <ds8600@naver.com>
2026-03-06 18:13:43 +0900

    chore(auth): add trailing slash to logout url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,8 +5,8 @@ from apps.users.views.auth import LoginAPIView, LogoutAPIView, SignupAPIView
 from .views.auth import EmailCodeSendAPIView
 
 urlpatterns = [
-    path("auth/signup", SignupAPIView.as_view(), name="auth-signup"),
-    path("auth/email/code", EmailCodeSendAPIView.as_view(), name="auth-email-code"),
-    path("auth/login", LoginAPIView.as_view(), name="auth-login"),
-    path("auth/logout", LogoutAPIView.as_view(), name="auth-logout"),
+    path("auth/signup/", SignupAPIView.as_view(), name="auth-signup"),
+    path("auth/email/code/", EmailCodeSendAPIView.as_view(), name="auth-email-code"),
+    path("auth/login/", LoginAPIView.as_view(), name="auth-login"),
+    path("auth/logout/", LogoutAPIView.as_view(), name="auth-logout"),
 ]
