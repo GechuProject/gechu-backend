9e57812df71ffdcbd4a9f8510147f5b8e5ea3b72
ds8000 <ds8600@naver.com>
2026-03-10 06:52:39 +0900

    docs(users): add swagger schema for me retrieve api

--- apps/users/views/me.py
+++ apps/users/views/me.py
@@ -1,5 +1,6 @@
 from typing import cast
 
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,6 +8,11 @@ from apps.users.models.user import User
 from apps.users.serializers.me import UserMeResponseSerializer
 
 
+@extend_schema(
+    tags=["Users"],
+    summary="내 정보 조회",
+    responses={200: UserMeResponseSerializer},
+)
 class UserMeRetrieveAPIView(generics.RetrieveAPIView[User]):
     permission_classes = [IsAuthenticated]
     serializer_class = UserMeResponseSerializer
