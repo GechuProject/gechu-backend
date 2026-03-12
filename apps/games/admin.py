481651b03ed87e07e2878d26b9466aaa0a92b772
ji-min0 <225418468+ji-min0@users.noreply.github.com>
2026-03-12 17:31:18 +0900

    chore: remove admin.py

--- apps/games/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Game
-
-
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
-    list_display = ("name", "esrb_rating", "age_rating_min", "released")
-    search_fields = ("name", "slug")
