a689bfaddf9f392a818309c6757c53c76c1d6671
ji-min0 <225418468+ji-min0@users.noreply.github.com>
2026-03-16 00:04:34 +0900

    fix: append trailing slash

--- apps/games/urls/admin_sync.py
+++ apps/games/urls/admin_sync.py
@@ -5,5 +5,5 @@ from apps.games.views.rawg_sync import RawgSyncView
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("sync/rawg/", RawgSyncView.as_view(), name="admin-rawg-sync"),
-    path("sync/igdb", IgdbSyncView.as_view(), name="admin-igdb-sync"),
+    path("sync/igdb/", IgdbSyncView.as_view(), name="admin-igdb-sync"),
 ]
